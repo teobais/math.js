f586e2e52e8ea0085dffaecd990bb786e21a4c63
Thodoris Bais <thodoris.bais@gmail.com>
2016-11-05 11:57:11 +0100

    Added format function and respective unit tests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ module.exports = {
 
     ceil: function(val) {
         return Math.ceil(val / 10) * 10;
+    },
+    format: function(val, decimals) {
+        return val.toFixed(decimals);
     }  
 
 }
--- test/index.js
+++ test/index.js
@@ -1,19 +1,38 @@
 const assert = require('assert');
-var math = require('../index'),
-    ceil = math.ceil;
+var math = require('../index');
 
 describe('#math.js', function() {
-    it('converts 13 to 20', function() {
-        assert.equal(ceil(13), 20 );
+    
+    describe("Testing ceil function", function () {
+        it('converts 13 to 20', function() {
+            assert.equal(math.ceil(13), 20 );
+        });
+    
+        it('converts 111 to 120', function() {
+            assert.equal(math.ceil(111), 120 );
+        });
+    
+        it('leaves 90 as is', function() {
+            assert.equal(math.ceil(90), 90 );
+        }); 
     });
     
-    it('converts 111 to 120', function() {
-        assert.equal(ceil(111), 120 );
-    });
     
-    it('leaves 90 as is', function() {
-        assert.equal(ceil(90), 90 );
-    });
+    describe("Testing format function", function() {
+        it('2 decimals', function() {
+            assert.equal(math.format(4.66364, 2), 4.66 );
+        });
+        
+        it('1 decimal', function() {
+            assert.equal(math.format(4.66364, 1), 4.7 );
+        });
+        
+        it('1 decimal', function() {
+            assert.equal(math.format(1.23e-10, 1), 0.0 );
+        });
+        
+    })
+    
 });
 
    
